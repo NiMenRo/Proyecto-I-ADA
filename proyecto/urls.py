65a638727052af8b28f9d85ca4b19bee2ea8e1f6
Victor Hernandez <victormanu968@gmail.com>
2024-10-07 22:51:27 -0500

    nav menu, items y rutas iniciales

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('terminal/', views.terminal_home, name='terminal_home'),
+    path('subasta/', views.subasta_home, name='subasta_home'),
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 # Pagina de inicio
 def index(request):
     return render(request, 'index.html', {'title': 'Hello','username': 'David', 'password': '1234'})
+
+
+
+def terminal_home(request):
+    return render(request, 'terminal_home.html', {'title': 'Terminal Inteligente','username': 'David', 'password': '1234'})
+
+def subasta_home(request):
+    return render(request, 'subasta_home.html', {'title': 'Subasta Inteligente','username': 'David', 'password': '1234'})
